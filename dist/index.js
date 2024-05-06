009ca3b572933be292153b6f83604f334bb49eda
Med Ali Khaled <medalikhaled331@gmail.com>
2024-05-06 18:01:58 +0100

    build dist

--- dist/index.js
+++ dist/index.js
@@ -34,6 +34,54 @@ const core = __importStar(__nccwpck_require__(9093));
 const github = __importStar(__nccwpck_require__(5942));
 const REQUIRED_PR_SECTIONS = ["description", "how to test it", "approach"];
 const isUI = false;
+//TODO: test this one
+/**
+ *
+ * @param {string} parsedMarkdown takes in a parsed markdown string
+ * @returns {Set<string>} returns all the heading titles h1, h2, h3 in a Set
+ */
+function getPrTitles(parsedMarkdown) {
+    // Regex for h1-h3 tags with capturing group
+    const regex = /<h[1-3]>(.*?)<\/h[1-3]>/gi;
+    const foundTitles = new Set();
+    let match;
+    while ((match = regex.exec(parsedMarkdown)) !== null) {
+        foundTitles.add(match[1].trim().toLowerCase());
+    }
+    return foundTitles;
+}
+//! last section is always ign
+function parseSections(parsedMarkdown) {
+    const sections = [];
+    let currentTitle = null;
+    let content = "";
+    // Regex for h1-h3 with capturing groups
+    const regex = /<h[1-3]>(.*?)<\/h[1-3]>(.*?)(?=<h1>|<h2|<h3|\b$)/gis;
+    let match;
+    while ((match = regex.exec(parsedMarkdown)) !== null) {
+        // Capture group 1: title, group 2: content
+        const title = match[1].trim();
+        const currentContent = match[2].trim();
+        if (currentTitle) {
+            sections.push({
+                title: currentTitle,
+                content: content || "",
+                characterCount: content.length,
+            });
+        }
+        currentTitle = title;
+        content = currentContent;
+    }
+    // Add the last section if content exists after the final heading
+    if (currentTitle) {
+        sections.push({
+            title: currentTitle,
+            content: content || "",
+            characterCount: content.length,
+        });
+    }
+    return sections;
+}
 async function run() {
     try {
         const token = core.getInput("gh-token", { required: true });
