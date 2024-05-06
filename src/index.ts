02acc3fdfbe037a623df84699e3c5f9683942a85
Med Ali Khaled <medalikhaled331@gmail.com>
2024-05-06 17:24:51 +0100

    parse pr

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-import { getInput } from "@actions/core";
-import { getOctokit } from "@actions/github";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import marked from "marked";
 
-const token = getInput("gh-token");
-const gh = getOctokit(token);
+async function run() {
+  try {
+    const token = core.getInput("gh-token", { required: true });
+    const octokit = github.getOctokit(token);
 
-console.log(`hello ${gh.request.toString()}`);
+    const context = github.context;
+    if (context.payload.pull_request == null) {
+      core.setFailed("No pull request found.");
+      return;
+    }
+
+    const prNumber = context.payload.pull_request.number;
+
+    const { data: pr } = await octokit.rest.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: prNumber,
+    });
+
+    const prDescription = pr.body;
+    console.log(prDescription);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
